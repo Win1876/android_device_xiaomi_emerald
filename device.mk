f1e86051e8421cb05e21a1520af9a12212776773
Win1876 <116477945+Win1876@users.noreply.github.com>
2025-02-12 18:10:30 +0300

    device.mk

--- device.mk
+++ device.mk
@@ -1,4 +1,4 @@
-#
+-#
 # Copyright (C) 2025 The Android Open Source Project
 # Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
 #
@@ -6,30 +6,28 @@
 #
 
 LOCAL_PATH := device/xiaomi/emerald
-# A/B
+
+# A/B OTA Post-Install Config
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
     POSTINSTALL_PATH_system=system/bin/otapreopt_script \
     FILESYSTEM_TYPE_system=ext4 \
     POSTINSTALL_OPTIONAL_system=true
 
-# Boot control HAL
+# Boot Control HAL (Use Shared Library Instead of Static HAL)
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.0-impl \
-    android.hardware.boot@1.0-service
-
-PRODUCT_PACKAGES += \
-    bootctrl.mt6789
-
-PRODUCT_STATIC_BOOT_CONTROL_HAL := \
+    android.hardware.boot@1.1-impl \
+    android.hardware.boot@1.1-service \
     bootctrl.mt6789 \
     libgptutils \
     libz \
     libcutils
 
+# A/B OTA Packages
 PRODUCT_PACKAGES += \
     otapreopt_script \
     cppreopts.sh \
     update_engine \
     update_verifier \
     update_engine_sideload
+
