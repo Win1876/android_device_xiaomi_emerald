fb9c92989b57fe0145da8be90dd1685a2692ab4e
Win1876 <116477945+Win1876@users.noreply.github.com>
2025-02-12 19:15:14 +0300

    device.mk

--- device.mk
+++ device.mk
@@ -1,11 +1,10 @@
--#
 # Copyright (C) 2025 The Android Open Source Project
 # Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
-LOCAL_PATH := device/xiaomi/emerald
+LOCAL_PATH := $(call my-dir)
 
 # A/B OTA Post-Install Config
 AB_OTA_POSTINSTALL_CONFIG += \
@@ -30,4 +29,3 @@ PRODUCT_PACKAGES += \
     update_engine \
     update_verifier \
     update_engine_sideload
-
