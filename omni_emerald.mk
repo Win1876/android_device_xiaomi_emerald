6b67a570a99c42b436ca604767e21c1361e47cc5
Win1876 <116477945+Win1876@users.noreply.github.com>
2025-02-13 10:35:35 +0300

    omni_emerald.mk

--- omni_emerald.mk
+++ omni_emerald.mk
@@ -10,7 +10,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product-if-exists, vendor/omni/config/common.mk)
+
+# Inherit from the device tree
+$(call inherit-product, device/xiaomi/emerald/device.mk)
 
 PRODUCT_DEVICE := emerald
 PRODUCT_NAME := omni_emerald
